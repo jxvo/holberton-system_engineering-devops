d929f116b2a2e1b7554c6f1997a90eb70daca3d3
Jonathan <16144089+jxvo@users.noreply.github.com>
2019-08-05 20:16:33 -0400

    create a file in /tmp using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create file “holberton” in /tmp
+file { '/tmp/holberton':
+    ensure  => file,
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
