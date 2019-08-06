29b6e1ac7ff6f99e77de2241fdd3f5b35dc9c2c3
Jonathan <16144089+jxvo@users.noreply.github.com>
2019-08-05 20:29:15 -0400

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #creates a manifest that kills a process
-exec { “killmenow”:
-    command => “pkill killmenow”,
-    path    => “/usr/bin”,
+exec { 'killmenow':
+    command => 'pkill killmenow',
+    path    => '/usr/bin',
 }
