8ccdebee85e8023416277b084af36b4f03edd8b6
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 14:09:52 +0000

    Query reddit API for subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Queries Reddit API 
-return number of suscribers to a subreddit
+Queries Reddit API
+returns number of suscribers to a subreddit
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ returns number of subscribers """
-    head = {'User-Agent': 'jank'}
+    head = {'User-Agent': 'me_irl'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     resp = requests.get(url, allow_redirects=False, headers=head)
     if resp.status_code >= 300:
