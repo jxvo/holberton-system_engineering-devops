27d9b852769adbdc7e8d6e66cde15b15d08230fe
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-09 04:08:29 +0000

    renamed file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,38 +1,19 @@
-# configure all the previous tasks but with Puppet
-
-$host_name = generate('/bin/uname', '--nodename')
-$nginx_site_config = "server {
-	listen 80 default_server;
-	listen [::]:80 default_server ipv6only=on;
-	root /var/www/html;
-	add_header X-Served-By ${host_name};
-	location /redirect_me {
-		return 301 http://example.com/;
-	}
-}"
-
-package { 'Nginx installation':
-  ensure => latest,
-  name   => 'nginx'
+#Header HTTP creation
+exec {'ensure update':
+command  => 'sudo apt update',
+provider => shell
 }
 
-file { 'Nginx site configuration':
-  ensure  => file,
-  require => Package['nginx'],
-  path    => '/etc/nginx/sites-available/default',
-  content => $nginx_site_config
+-> package {'nginx':
+  ensure => 'installed'
+  }
+
+-> file_line {'header':
+path  => '/etc/nginx/nginx.conf',
+match => 'http {',
+line  => "http {\n\tadd_header X-Served-By ${hostname};"
 }
 
-file { 'site index':
-  ensure  => file,
-  require => Package['nginx'],
-  path    => '/var/www/html/index.html',
-  content => "Holberton School\n"
-}
-
-service { 'Nginx service':
-  ensure    => running,
-  name      => 'nginx',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default']
+-> exec {'start':
+command => '/usr/sbin/service nginx restart'
 }
